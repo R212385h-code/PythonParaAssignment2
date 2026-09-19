a9609c56bd445dee41b3a4f41e0f1302e7182d68
R212385h-code <R212385h@cuz.ac.zw>
2026-09-19 18:53:56 +0200

    Update Question6.py

--- Question6.py
+++ Question6.py
@@ -1,1 +1,20 @@
+class Vehicle:
+    def move(self):
+        print("The vehicle is moving")
 
+
+class Car(Vehicle):
+    def move(self):
+        print("The car is driving")
+
+
+class Bike(Vehicle):
+    def move(self):
+        print("The bike is riding")
+
+
+car = Car()
+bike = Bike()
+
+car.move()
+bike.move()
