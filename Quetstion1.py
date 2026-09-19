c5ea9a23be3a8656d2ab866a5f547960b64882f1
R212385h-code <R212385h@cuz.ac.zw>
2026-09-19 17:00:40 +0200

    Quetstion1.py

--- Quetstion1.py
+++ Quetstion1.py
@@ -1,1 +1,28 @@
+import sqlite3
 
+connection = sqlite3.connect("students.db")
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    student TEXT,
+    course TEXT,
+    mark INTEGER
+)
+""")
+
+cursor.execute("""
+INSERT INTO students (student, course, mark)
+VALUES (?, ?, ?)
+""", ("John", "Computer Science", 85))
+
+connection.commit()
+
+cursor.execute("SELECT * FROM students")
+students = cursor.fetchall()
+
+for student in students:
+    print(student)
+
+connection.close()
