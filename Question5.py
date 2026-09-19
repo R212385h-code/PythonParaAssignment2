88c233d22a9245164006e2b3ad29b7462bbcc1b4
R212385h-code <R212385h@cuz.ac.zw>
2026-09-19 17:19:52 +0200

    Update Question5.py

--- Question5.py
+++ Question5.py
@@ -1,1 +1,38 @@
+from abc import ABC, abstractmethod
 
+class FileHandler(ABC):
+
+    @abstractmethod
+    def read(self):
+        pass
+
+    @abstractmethod
+    def write(self, data):
+        pass
+
+
+class TextFileHandler(FileHandler):
+
+    def read(self):
+        print("Reading text file")
+
+    def write(self, data):
+        print("Writing:", data)
+
+
+class BinaryFileHandler(FileHandler):
+
+    def read(self):
+        print("Reading binary file")
+
+    def write(self, data):
+        print("Writing binary data:", data)
+
+
+text = TextFileHandler()
+text.read()
+text.write("Hello")
+
+binary = BinaryFileHandler()
+binary.read()
+binary.write("101010")
