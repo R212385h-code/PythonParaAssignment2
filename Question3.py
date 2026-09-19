d3f138790e831f759730a72b7a9910ff3d8b4e50
R212385h-code <R212385h@cuz.ac.zw>
2026-09-19 17:15:22 +0200

    Update Question3.py

--- Question3.py
+++ Question3.py
@@ -1,1 +1,17 @@
+import socket
 
+server = socket.socket()
+server.bind(("localhost", 5000))
+server.listen(1)
+
+print("Waiting for client...")
+
+try:
+    connection, address = server.accept()
+    message = connection.recv(1024).decode()
+    print("Message:", message)
+    connection.close()
+except:
+    print("Connection error")
+
+server.close()
